f6f3d81e0d9a8e979ce4985f6b3098b21f85eb04
Mateo Mercado <matemerc@gmail.com>
2024-09-02 17:41:01 -0400

    Initial Commit

---
+++ Fipe/FipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FipeApp.swift
+//  Fipe
+//
+//  Created by Mateo Mercado on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
